fcac40e9983e22137522d51426901c3c35c13673
Leslie Restrepo <leslierestrepo@gmail.com>
2016-03-15 10:51:37 +0400

    Update when api does not bring results

--- javascript/js.js
+++ javascript/js.js
@@ -3,9 +3,12 @@ $(document).ready(function(){
 		if(event.which == 13){ 
 			var url= validateUrl($(this).val());    		
 		    $.getJSON("https://api.spotify.com/v1/search?q="+url+"&type=track", function(data){
-
-		    	var template = Handlebars.compile($("#songsList").html());
-		    	$(".songs").html(template({items: data.tracks.items}));
+		    	if(data.tracks.items.length > 0){
+			    	var template = Handlebars.compile($("#songsList").html());
+			    	$(".songs").html(template({items: data.tracks.items}));
+			    }else{
+		    		$(".songs").html("<h3>No se encontraron resultados</h3>");
+			    }
 		    });
     	}
 	});
